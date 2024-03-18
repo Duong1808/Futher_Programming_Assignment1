fd7cbaf2f5a6dfd108dd4a2405ea7815ed86f840
Duong1808 <S3965530@rmit.edu.vn>
2024-03-19 00:38:15 +0700

    commit

---
+++ Further_Programming/src/Customer.java
@@ -0,0 +1,54 @@
+import java.util.List;
+
+public class Customer {
+    private String id;
+    private String fullName;
+    private InsuranceCard insuranceCard;
+    private List<Claim> claims;
+
+    // Constructor
+    public Customer(String id, String fullName, InsuranceCard insuranceCard, List<Claim> claims) {
+        this.id = id;
+        this.fullName = fullName;
+        this.insuranceCard = insuranceCard;
+        this.claims = claims;
+    }
+
+    // Gtters & Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public InsuranceCard getInsuranceCard() {
+        return insuranceCard;
+    }
+
+    public List<Claim> getClaims() {
+        return claims;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setInsuranceCard(InsuranceCard insuranceCard) {
+        this.insuranceCard = insuranceCard;
+    }
+
+    public void setClaims(List<Claim> claims) {
+        this.claims = claims;
+    }
+
+    public void display() {
+
+    }
+}
